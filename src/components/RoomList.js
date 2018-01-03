ca77936685eb81b30074d5542fe8af32f82ace80
Isaac Simon <ijsimon@sbcglobal.net>
2018-01-03 16:13:07 -0600

    Creating rooms works

--- src/components/RoomList.js
+++ src/components/RoomList.js
@@ -4,11 +4,24 @@ export class RoomList extends Component {
   constructor(props) {
     super(props);
       this.state = {
+        name: "",
         rooms: []
       };
 
     this.roomsRef = this.props.firebase.database().ref('rooms');
+    this.handleChange = this.handleChange.bind(this);
+    this.createRoom = this.createRoom.bind(this);
   }
+
+  handleChange(e) {
+  this.setState({ name: e.target.value });
+}
+
+createRoom(e) {
+  this.roomsRef.push({ name: this.state.name });
+  this.setState({ name: "" });
+}
+
 
   componentDidMount() {
     this.roomsRef.on('child_added', snapshot => {
@@ -22,8 +35,19 @@ export class RoomList extends Component {
     const roomList = this.state.rooms.map((room) =>
       <li key={room.key}>{room.name}</li>
     );
+
+    const roomForm = (
+    <form onSubmit={this.createRoom}>
+      <input type="text" value={this.state.name} placeholder="Enter Room Name" onChange={this.handleChange}/>
+      <input type="submit" value="Create" />
+    </form>
+  );
+
     return(
+      <div>
+      {roomForm}
       <ul>{roomList}</ul>
+      </div>
     );
   }
 }
